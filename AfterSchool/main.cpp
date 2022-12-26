9fffad8a25ee5399e9856b32f1d9d8554a53efef
minseo kim <halia0709@gmail.com>
2022-12-26 14:16:52 +0900

    플레이어 이동

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -5,16 +5,40 @@ int main(void)
 {
 	//윈도우창 생성
 	RenderWindow window(VideoMode(640,480),"AfterSchool");
+	window.setFramerateLimit(60);
+	RectangleShape player;
+	player.setSize(Vector2f(40,40));
+	player.setPosition(100, 100);
+	player.setFillColor(Color::Red);
 	//윈도우가 열려있을 때까지 반복
 	while (window.isOpen()) 
 	{
 		Event event;
 		while (window.pollEvent(event)) {
 			//종료(X) 버튼을 누르면
-			if (event.type == Event::Closed)
+			switch (event.type) 
+			{
+			case Event::Closed:
 				window.close();//윈도우 닫는다.
+			}
+			
+				
 		}
-		
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			player.move(-1,0);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			player.move(1, 0);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			player.move(0, -1);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			player.move(0, 1);
+		}
+
+		window.draw(player);
+		window.display();
 	}
 	
 	return 0;
