509f460865c08422a46459376b1027276ca57f8d
minseo kim <halia0709@gmail.com>
2022-12-26 11:37:14 +0900

    sfml 기본틀 완성

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
-
-int main(void) {
-	printf("hello world");
+#include <SFML/Graphics.hpp>
+using namespace sf;
+int main(void) 
+{
+	//윈도우창 생성
+	RenderWindow window(VideoMode(640,480),"AfterSchool");
+	//윈도우가 열려있을 때까지 반복
+	while (window.isOpen()) 
+	{
+		Event event;
+		while (window.pollEvent(event)) {
+			//종료(X) 버튼을 누르면
+			if (event.type == Event::Closed)
+				window.close();//윈도우 닫는다.
+		}
+		
+	}
+	
 	return 0;
 }
