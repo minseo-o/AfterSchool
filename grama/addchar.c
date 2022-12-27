a4aad7d2866fc8d3c74ace5b134dddd9f0ad9f8f
minseo kim <halia0709@gmail.com>
2022-12-27 11:15:10 +0900

    [문법] 문자열 덧붙이기 4

--- grama/addchar.c
+++ grama/addchar.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h>//문자열 라이브러리
+#include <stdlib.h>// itoa사용
 int main() {
     
 
     char str[30] = "score : ";
-    char str2[30] = "300";
+    int score = 300;
+    char str2[30];
+ 
+    //정수를 문자열로
+    itoa(score, str2, 10);
+
     
     strcat(str, str2);
     printf("%s", str);
