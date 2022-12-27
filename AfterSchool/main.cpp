a655e51f497fe3de45a9c065b6fdf1bae90ec881
minseo kim <halia0709@gmail.com>
2022-12-27 15:30:31 +0900

    enemy 구조체화

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -1,168 +1,175 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <SFML/Graphics.hpp>
+#include <stdlib.h>
 #include <time.h>
 #include <SFML/Audio.hpp>
 
 using namespace sf;
+
 struct Player {
 	RectangleShape sprite;
 	int speed;
 	int score;
-
 };
-int main(void) {
 
-	srand((unsigned int)time(NULL));//랜덤 함수 사용
-	long start_time = clock();
-	long spent_time;
+struct Enemy {
+	RectangleShape sprite;
+	int speed;
+	int life;
+	int score;
+	SoundBuffer explosion_buffer;
+	Sound explosion_sound;
+};
 
-
-	//640 x 480 윈도우 화면 나옴
-	//잠깐 떴다가 사라지는 건 return 0때문에 프로그램이 종료된 것
+int main(void)
+{
+	// 윈도창 생성
 	RenderWindow window(VideoMode(640, 480), "AfterSchool");
-	window.setFramerateLimit(60);//1초에 60장 보여준다. 플레이어가 빨리 가지 않도록 하기
+	window.setFramerateLimit(60);
 
-	//폰트
+	srand(time(0));
+
+	long start_time = clock();	// 게임 시작시간
+	long spent_time;			// 게임 진행시간
+
+	// text
 	Font font;
-	font.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf");//C드라이브에 있는 폰트 가져오기
+	font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf");
 
 	Text text;
+	text.setFont(font);
+	text.setCharacterSize(30);		// 글자크기 조절
+	text.setFillColor(Color(255, 255, 255));
+	text.setPosition(0, 0);
 	char info[40];
-	text.setFont(font);//폰트 세팅
-	text.setCharacterSize(24);//폰트 크기
-	text.setFillColor(Color(255, 255, 255));//RGB로 흰색 표현
-	text.setPosition(0, 0);//텍스트 위치 0,0
 
-	//배경
+	// 배경
 	Texture bg_texture;
-	bg_texture.loadFromFile("./resources/images/background.png");
+	bg_texture.loadFromFile("./resources/images/background.jpg");
 	Sprite bg_sprite;
 	bg_sprite.setTexture(bg_texture);
 	bg_sprite.setPosition(0, 0);
 
-	//네모 모양의 플레이어
-
+	// 플레이어
 	struct Player player;
-	player.sprite.setSize(Vector2f(40, 40));//플레이어 사이즈
-	player.sprite.setPosition(100, 100);//플레이어 시작 위치
-	player.sprite.setFillColor(Color::Red);//플레이어 색상
-	player.speed = 10;//플레이어 속도
-	player.score = 0;//플레이어 점수
+	player.sprite.setSize(Vector2f(40, 40));
+	player.sprite.setPosition(100, 100);
+	player.sprite.setFillColor(Color::Red);
+	player.speed = 5;
+	player.score = 0;
 
-	//적
-	const int ENEMY_NUM = 20;
-	RectangleShape enemy[ENEMY_NUM];
-	int enemy_life[ENEMY_NUM];//적의 체력
-	int enemy_score = 100;//적을 잡을 때마다 얻는 점수
-	int enemy_speed[ENEMY_NUM];
-	//적 잡을 때 나는 소리 
-	SoundBuffer enemy_explosion_buffer;
-	enemy_explosion_buffer.loadFromFile("./resources/sounds/rumble.flac");
-	Sound enemy_explosion_sound;
-	enemy_explosion_sound.setBuffer(enemy_explosion_buffer);
+	// 적(enemy)
+	const int ENEMY_NUM = 10;
 
+	struct Enemy enemy[ENEMY_NUM];
+
+	// enemy 초기화
 	for (int i = 0; i < ENEMY_NUM; i++)
 	{
-		enemy[i].setSize(Vector2f(70, 70));
-		enemy[i].setPosition(rand() % 300 + 300, rand() % 410);
-		enemy_life[i] = 1;
-		enemy_speed[i] = -(rand() % 5 + 1);
-		enemy[i].setFillColor(Color::Yellow);//적 색상
+		// TODO : 굉장히 비효율적인 코드이므로 나중에 refactoring
+		enemy[i].explosion_buffer.loadFromFile("./resources/sounds/rumble.flac");
+		enemy[i].explosion_sound.setBuffer(enemy[i].explosion_buffer);
+		enemy[i].score = 100;
+
+		enemy[i].sprite.setSize(Vector2f(70, 70));
+		enemy[i].sprite.setFillColor(Color::Yellow);
+		enemy[i].sprite.setPosition(rand() % 300 + 300, rand() % 380);
+		enemy[i].life = 1;
+		enemy[i].speed = -(rand() % 10 + 1);
 	}
 
 
-	//유지 시키는 방법은? -> 무한 반복
-	while (window.isOpen()) //윈도우창이 열려있는 동안 계속 반복
+	// 윈도가 열려있을 때까지 반복
+	while (window.isOpen())
 	{
-		Event event;//이벤트 생성
-		while (window.pollEvent(event)) //이벤트가 발생. 이벤트가 발생해야 event 초기화가 됨
+		Event event;
+		while (window.pollEvent(event))
 		{
 			switch (event.type)
 			{
-				//종료(x)버튼을 누르면 Event::Closed(0) 
-			case Event::Closed://정수임
-				window.close();//윈도우창이 닫힘
+				// 종료(x) 버튼을 누르면 Event::Closed(0)
+			case Event::Closed:
+				window.close();		// 윈도를 닫는다
 				break;
-				//키보드를 눌렀을 때 
+				// 키보드를 눌렀을 때(누른 순간만을 감지)
 			case Event::KeyPressed:
-				//case문 안에 변수를 선언할 때에는 중괄호를 쳐야 함
 			{
-				//space키 누르면 모든 enemy 다시 출현
+				// 스페이스 키 누르면 모든 enemy 다시 출현
 				if (event.key.code == Keyboard::Space)
 				{
 					for (int i = 0; i < ENEMY_NUM; i++)
 					{
-						enemy[i].setSize(Vector2f(70, 70));
-						enemy[i].setPosition(rand() % 300 + 300, rand() % 410);
-						enemy_life[i] = 1;
-						enemy[i].setFillColor(Color::Yellow);//적 색상
-						enemy_speed[i] = -(rand() % 5 + 1);
+						enemy[i].sprite.setSize(Vector2f(70, 70));
+						enemy[i].sprite.setFillColor(Color::Yellow);
+						enemy[i].sprite.setPosition(rand() % 300 + 300, rand() % 380);
+						enemy[i].life = 1;
+						enemy[i].speed = -(rand() % 10 + 1);
 					}
 				}
 				break;
 			}
+
 			}
 		}
-		spent_time = clock()-start_time;
-		//방향키
+
+		spent_time = clock() - start_time;
+
+		// 방향키 start
 		if (Keyboard::isKeyPressed(Keyboard::Left))
 		{
-			player.sprite.move(-1 * player.speed, 0);//왼쪽 이동
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-		{
-			player.sprite.move(0, -1 * player.speed);//위쪽 이동
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Down))
-		{
-			player.sprite.move(0, player.speed);//아래쪽 이동
+			player.sprite.move(-player.speed, 0);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Right))
 		{
-			player.sprite.move(player.speed, 0);//오른쪽 이동
+			player.sprite.move(player.speed, 0);
 		}
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+		{
+			player.sprite.move(0, -player.speed);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+		{
+			player.sprite.move(0, player.speed);
+		}	// 방향키 end
 
-		//enemy와의 충돌
-		//intersects : 플레이어와 적 사이에서 교집합이 있는가
+
+
 		for (int i = 0; i < ENEMY_NUM; i++)
 		{
-			if (enemy_life[i] > 0)
+			if (enemy[i].life > 0)
 			{
-				//enemy와 충돌
-				if (player.sprite.getGlobalBounds().intersects(enemy[i].getGlobalBounds()))
+				// enemy와의 충돌
+				if (player.sprite.getGlobalBounds().intersects(enemy[i].sprite.getGlobalBounds()))
 				{
-					printf("enemy[%d]와의 충돌\n", i);
-					enemy_life[i] -= 1;//적의 생명 줄이기
-					player.score += enemy_score;
-					//TODO : 코드 refactoring 필요
-					if (enemy_life[i] == 0) {
-						enemy_explosion_sound.play();
+					printf("enemy[%d]과 충돌\n", i);
+					enemy[i].life -= 1;
+					player.score += enemy[i].score;
 
+					// TODO : 코드 refactoring 필요
+					if (enemy[i].life == 0)
+					{
+						enemy[i].explosion_sound.play();
 					}
 				}
-				enemy[i].move(enemy_speed[i],0);
+				enemy[i].sprite.move(enemy[i].speed, 0);
 			}
-			
 		}
-		
-		sprintf(info, "score: %d  time : %d", player.score, (spent_time)/1000);
+
+		sprintf(info, "score:%d time:%d"
+			, player.score, spent_time / 1000);
 		text.setString(info);
-		
-		
-		window.clear(Color::Black);//플레이어 자체 제거 (배경 지우기)
+
+		window.clear(Color::Black);
 		window.draw(bg_sprite);
+
+		// draw는 나중에 호출할수록 우선순위가 높아짐
 		for (int i = 0; i < ENEMY_NUM; i++)
-		{
-			if (enemy_life[i] > 0)  window.draw(enemy[i]);//적 보여주기
-		}
-		//화면이 열려져 있는 동안 계속 그려야 함
-		//draw는 나중에 호출할수록 우선순위가 높아짐
-		
-		window.draw(player.sprite);//플레이어 보여주기(그려주기)
+			if (enemy[i].life > 0)
+				window.draw(enemy[i].sprite);
+		window.draw(player.sprite);
 		window.draw(text);
-		
+
+
 		window.display();
 	}
 
