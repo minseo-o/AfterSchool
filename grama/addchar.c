77b4e18e45ab4a670eda510b9dcfdc052f1c61a0
minseo kim <halia0709@gmail.com>
2022-12-27 10:48:34 +0900

    [문법] 문자열 덧붙이기 3

--- grama/addchar.c
+++ grama/addchar.c
@@ -4,17 +4,14 @@ int main() {
     
 
     char str[30] = "score : ";
-    int str_len = strlen(str);
-    
-
     char str2[30] = "300";
-    int str2_len = strlen(str2);
+    
+    strcat(str, str2);
+    printf("%s", str);
+
     
 
-    for (int i = 0; i < str2_len; i++)
-        str[i + str_len] = str2[i];
-    str[str_len + str2_len] = '\0';
-    printf("%s", str);
+ 
 
      
     return 0;
