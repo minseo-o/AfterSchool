0573842132b76a9f30feea9d1f31b2168a9ddd17
minseo kim <halia0709@gmail.com>
2022-12-27 10:38:28 +0900

    문자열 덧붙이기

---
+++ grama/addchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    
+
+    char str[30] = "score : ";
+    int str_len = 0;
+    while (str[str_len] != '\0')
+        str_len++;
+
+    char str2[30] = "300";
+    int str2_len = 0;
+    while (str[str2_len] != '\0')
+        str2_len++;
+
+    for (int i = 0; i < str2_len; i++)
+        str[i + str_len] = str2[i];
+    str[str_len + str2_len] = '\0';
+    printf("%s", str);
+
+     
+    return 0;
+
+}
