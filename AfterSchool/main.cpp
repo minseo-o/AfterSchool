780bcfd22d8164104d0de8c16232edb8512bded0
minseo kim <halia0709@gmail.com>
2022-12-28 13:47:53 +0900

    코드 refactor(충돌함수)

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -30,6 +30,10 @@ struct Bullet {
 	int is_fire;
 
 };
+//obj1과 obj2의 충돌하면 1반환, 충돌 안하면 0 반환 
+int is_collide(RectangleShape obj1, RectangleShape obj2) {
+	return obj1.getGlobalBounds().intersects(obj2.getGlobalBounds());
+}
 //전역변수
 const int ENEMY_NUM = 10;
 const int W_WIDTH = 1200, W_HEIGHT = 600;
@@ -192,9 +196,10 @@ int main(void)
 			{
 				//TODO : 총알이 관통하는 버그 수정할 것 
 				// enemy와의 충돌
-				if (player.sprite.getGlobalBounds().intersects(enemy[i].sprite.getGlobalBounds())|| bullet.sprite.getGlobalBounds().intersects(enemy[i].sprite.getGlobalBounds()))
+				if (is_collide(player.sprite, enemy[i].sprite) || is_collide(bullet.sprite, enemy[i].sprite))
+					
 				{
-					printf("enemy[%d]과 충돌\n", i);
+				
 					enemy[i].life -= 1;
 					player.score += enemy[i].score;
 
